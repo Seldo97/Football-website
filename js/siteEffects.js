7289aaf3384eca7d5c99ddedf073cca4947423c8
Seldo97 <seldo97@gmail.com>
2020-01-22 19:54:04 +0100

    wydzielenie kodu ajax do oddzielnego pliku

--- js/siteEffects.js
+++ js/siteEffects.js
@@ -16,3 +16,28 @@ $(document).click(function() {
 $(document).ready(function(){
     $(".alert").slideToggle(500).delay(3500).slideToggle(500);
 });
+
+$(document).ready(function(){
+    $(document).on('click', '.edit_data', function() {
+        var id_druzyna = $(this).attr("id");
+        var url_bezp = $(this).attr("url");
+        var url = url_bezp;
+        //alert(url_bezp);
+        $.when(
+            $.ajax({
+                type: 'GET',
+                url: url,
+                success: function(response){
+                    res=response;
+                    success=true;
+                },
+                error:function(){
+                    //handle error
+                },
+            })).then(function(){
+                if(success){
+                    $("#myModal").html(res).modal("show");
+                }
+        });
+    });
+});
